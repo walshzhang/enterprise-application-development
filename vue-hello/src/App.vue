be80cfb17626dab70efd6af9492afeda7923424c
walsh <walsh.zhang@hotmail.com>
2020-08-21 19:52:12 +0800

    first vue app

---
+++ vue-hello/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h4>Counter: {{ counter }}</h4>
+    <button v-on:click="increase">点一次加1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return ({
+      counter: 0
+    })
+  },
+  methods: {
+    increase() {
+      this.counter++;
+    }
+  }
+}
+</script>
