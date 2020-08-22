bf6ea1d28ca2c414321112b7ffc32910baa070c8
walsh <walsh.zhang@hotmail.com>
2020-08-23 00:03:48 +0800

    format code

--- vue-single-component/src/App.vue
+++ vue-single-component/src/App.vue
@@ -6,35 +6,65 @@
     <div id="buttons">
 
       <button @click="handleChangeView('PureHtml')"
-              :class="{'selected': currentView === 'PureHtml'}">纯静态内容组件</button>
+              :class="{'selected': currentView === 'PureHtml'}">
+        纯静态内容组件
+      </button>
       <button @click="handleChangeView('SimpleTextInterpolation')"
-              :class="{'selected': currentView === 'SimpleTextInterpolation'}">简单文本插值</button>
+              :class="{'selected': currentView === 'SimpleTextInterpolation'}">
+        简单文本插值
+      </button>
       <button @click="handleChangeView('JSExpressionInterpolation')"
-              :class="{'selected': currentView === 'JSExpressionInterpolation'}">JS表达式插值</button>
+              :class="{'selected': currentView === 'JSExpressionInterpolation'}">
+        JS表达式插值
+      </button>
       <button @click="handleChangeView('VBindDirectives')"
-              :class="{'selected': currentView === 'VBindDirectives'}">v-bind指令</button>
+              :class="{'selected': currentView === 'VBindDirectives'}">
+        v-bind指令
+      </button>
       <button @click="handleChangeView('VIfDirectives')"
-              :class="{'selected': currentView === 'VIfDirectives'}">v-if指令</button>
+              :class="{'selected': currentView === 'VIfDirectives'}">
+        v-if指令
+      </button>
       <button @click="handleChangeView('VElseDirectives')"
-              :class="{'selected': currentView === 'VElseDirectives'}">v-else指令</button>
+              :class="{'selected': currentView === 'VElseDirectives'}">
+        v-else指令
+      </button>
       <button @click="handleChangeView('VForDirectives')"
-              :class="{'selected': currentView === 'VForDirectives'}">v-for指令：待办事项</button>
+              :class="{'selected': currentView === 'VForDirectives'}">
+        v-for指令：待办事项
+      </button>
       <button @click="handleChangeView('VFor2Directives')"
-              :class="{'selected': currentView === 'VFor2Directives'}">v-for指令：学生缺勤</button>
+              :class="{'selected': currentView === 'VFor2Directives'}">
+        v-for指令：学生缺勤
+      </button>
       <button @click="handleChangeView('VOnDirective')"
-              :class="{'selected': currentView === 'VOnDirective'}">v-on指令：表达式逻辑</button>
+              :class="{'selected': currentView === 'VOnDirective'}">
+        v-on指令：表达式逻辑
+      </button>
       <button @click="handleChangeView('VOn2Directive')"
-              :class="{'selected': currentView === 'VOn2Directive'}">v-on指令：处理逻辑封装进方法</button>
+              :class="{'selected': currentView === 'VOn2Directive'}">
+        v-on指令：处理逻辑封装进方法
+      </button>
       <button @click="handleChangeView('VOn3Directive')"
-              :class="{'selected': currentView === 'VOn3Directive'}">v-on指令：内联方法调用</button>
+              :class="{'selected': currentView === 'VOn3Directive'}">
+        v-on指令：内联方法调用
+      </button>
       <button @click="handleChangeView('VOn4Directive')"
-              :class="{'selected': currentView === 'VOn4Directive'}">v-on指令：内联方法调用2</button>
+              :class="{'selected': currentView === 'VOn4Directive'}">
+        v-on指令：内联方法调用2
+      </button>
       <button @click="handleChangeView('VModelDirective')"
-              :class="{'selected': currentView === 'VModelDirective'}">v-model指令：文本框</button>
+              :class="{'selected': currentView === 'VModelDirective'}">
+        v-model指令：文本框
+      </button>
       <button @click="handleChangeView('VModel2Directive')"
-              :class="{'selected': currentView === 'VModel2Directive'}">v-model指令：多行文本</button>
+              :class="{'selected': currentView === 'VModel2Directive'}">
+        v-model指令：多行文本
+      </button>
       <button @click="handleChangeView('VModel3Directive')"
-              :class="{'selected': currentView === 'VModel3Directive'}">v-model指令：选择框（单选）</button>
+              :class="{'selected': currentView === 'VModel3Directive'}">
+        v-model指令：选择框（单选）
+      </button>
     </div>
   </div>
 </template>
