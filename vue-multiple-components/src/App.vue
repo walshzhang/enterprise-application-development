ebbed5bae7bc2beee6cb43591567be5a828dfde3
walsh <walsh.zhang@hotmail.com>
2020-08-24 22:22:12 +0800

    创建多组件应用示例

---
+++ vue-multiple-components/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <counter style="margin-right: 5px; "/>
+    <counter />
+  </div>
+</template>
+<script>
+const counter = {
+  data() {
+    return {count: 0};
+  },
+  template: `
+    <button @click="inc">我被点击了 {{ count }} 次</button>`,
+  methods: {
+    inc() {
+      this.count++;
+    }
+  }
+};
+
+export default {
+  components: {
+    counter
+  }
+}
+</script>
