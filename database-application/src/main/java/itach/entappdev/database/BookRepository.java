3a39bb90de2e763b4caed94d5be8e2b7ce42e350
walsh <walsh.zhang@hotmail.com>
2020-09-12 16:55:57 +0800

    remove database-application project

--- database-application/src/main/java/itach/entappdev/database/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package itach.entappdev.database;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, String> {
-}
--- database-application/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS books;
-CREATE TABLE books
-(
-    id    varchar(32)         not null,
-    title varchar(50) not null,
-    price double      not null,
-    primary key (id)
-);
