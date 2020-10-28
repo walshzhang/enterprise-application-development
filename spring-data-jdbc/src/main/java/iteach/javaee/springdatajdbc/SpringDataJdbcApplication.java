0494f8b43f7d77fcd839ceff1f5f1bf24f07ffeb
walsh <walsh.zhang@hotmail.com>
2020-10-28 11:02:58 +0800

    Spring Data JDBC: init version

---
+++ spring-data-jdbc/src/main/java/iteach/javaee/springdatajdbc/SpringDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package iteach.javaee.springdatajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJdbcApplication.class, args);
+	}
+
+}
